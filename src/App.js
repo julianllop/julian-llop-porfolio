169771788a99741f08af77973f825966682dd81a
Julian Llop <juliianllop@gmail.com>
2024-06-17 00:47:58 -0300

    new update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import Presentation from "./components/presentation/presentation";
-import style from './app.module.css'
+import style from "./app.module.css";
 import About from "./components/about/about";
 import Skills from "./components/skills/skills";
 import Projects from "./components/projects/projects";
 
-
 function App() {
-  return (
-    <div className={style.App}>
-      <Presentation/>
-      <About/>
-      <Skills/>
-      <Projects/>
-    </div>
-  );
+    return (
+        <div className={style.App}>
+            <div className={style.shapeBG}></div>
+            <Presentation />
+            <About />
+            <Projects />
+            <Skills />
+        </div>
+    );
 }
 
 export default App;
